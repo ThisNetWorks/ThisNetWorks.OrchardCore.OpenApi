4f43155429eb55d07a069e259c8d7bcd4e2b4b5f
Dean Marcussen <dean.marcussen@gmail.com>
2021-08-29 12:36:35 +0100

    Upgrade to Orchard Core Preview

--- src/ThisNetWorks.OrchardCore.OpenApi/Manifest.cs
+++ src/ThisNetWorks.OrchardCore.OpenApi/Manifest.cs
@@ -5,7 +5,7 @@ using OrchardCore.Modules.Manifest;
     Name = "OpenAPI Tools",
     Author = "ThisNetWorks",
     Website = "https://github.com/ThisNetWorks",
-    Version = "1.0.0",
+    Version = "1.1.0",
     Description = "Provides a dynamic OpenAPI definition for Orchard Core Content Types and REST API Controllers",
     Category = "Content Management"
 )]
